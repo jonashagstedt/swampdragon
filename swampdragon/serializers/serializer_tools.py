bd1e84d692514a8e6ad5fc39880ca81403d07707
Julio Gutierrez <lcc.julio@gmail.com>
2015-04-03 00:31:01 -0600

    Update serializer_tools.py

--- swampdragon/serializers/serializer_tools.py
+++ swampdragon/serializers/serializer_tools.py
@@ -12,7 +12,7 @@ class FieldType(namedtuple('FieldType', 'field, model, fk, m2m')):
     '''
     @property
     def is_m2m(self):
-        return self.fk is False and self.m2m is True and isinstance(self.field, RelatedObject)
+        return self.fk is False and self.m2m is True and isinstance(self.field, ForeignObjectRel)
 
     @property
     def is_reverse_m2m(self):
@@ -24,7 +24,7 @@ class FieldType(namedtuple('FieldType', 'field, model, fk, m2m')):
 
     @property
     def is_reverse_fk(self):
-        return self.fk is False and self.m2m is False and isinstance(self.field, RelatedObject)
+        return self.fk is False and self.m2m is False and isinstance(self.field, ForeignObjectRel)
 
 
 def get_serializer_relationship_field(serializer, related_serializer):
